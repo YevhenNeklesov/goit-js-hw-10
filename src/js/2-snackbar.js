c42cc02a451a20b7dcd35840002a8f409809fb55
YevhenNeklesov <161756863+YevhenNeklesov@users.noreply.github.com>
2024-08-01 11:46:26 +0200

    add snackbar

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,46 @@
+
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+
+
+const snackbarForm = document.querySelector(".form");
+const delayValue = document.querySelector("input[name='delay']");
+const selectedState = document.querySelector("input[name='state']");
+const submitBtn = document.querySelector("button[type='submit']");
+let promise;
+snackbarForm.addEventListener("submit", onSubmitHadler);
+
+function onSubmitHadler(event) {
+    event.preventDefault();
+    let state = event.target.elements.state.value;
+    let delay = event.target.elements.delay.value;
+
+    promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (state === 'fulfilled') {
+            resolve(`✅ Fulfilled promise in ${delay}ms`)
+            } else { reject(`❌ Rejected promise in ${delay}ms`) }
+        }, delay);
+       })
+
+    
+    
+    
+    promise
+        .then(value => iziToast.success({
+            message: `${value}`,
+            position: 'topRight',
+            timeout: 2000,
+            icon: '',
+            }))
+        .catch(error => iziToast.error({
+            message: `${error}`,
+            position: 'topRight',
+            timeout: 2000,
+            icon: '',
+        }));
+    
+    event.target.reset();
+}
